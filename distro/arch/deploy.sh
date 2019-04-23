69f38d6a707783365d1db3f652bccc396c4ed9d3
Max <chestm007@hotmail.com>
2019-04-23 23:39:34 +1000

    .

---
+++ distro/arch/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+# Set up to run makepkg
+wget https://www.archlinux.org/packages/core/x86_64/pacman/download/ -O pacman.pkg.tar.xz
+tar -Jxf pacman.pkg.tar.xz
+bindir="$(pwd)/usr/bin"
+export PATH="$bindir:$PATH"
+export LIBRARY="$(pwd)/usr/share/makepkg"
+config="$(pwd)/etc/makepkg.conf"
+
+
+# Get the repo
+git config --global --add core.sshCommand "ssh -o StrictHostKeyChecking=false -i /tmp/aurkey"
+git clone ssh://aur@aur.archlinux.org/amdgpu-fan.git aur
+
+# Update it
+cp PKGBUILD aur/
+cd aur
+/bin/bash "$bindir/makepkg" --config="$config" --printsrcinfo >.SRCINFO
+
+# Commit
+git add PKGBUILD .SRCINFO
+git config user.email "chestm007+aurci@hotmail.com"
+git config user.name "chestm007-ci"
+git commit -m "Release ${CIRCLE_TAG}"
+
+# Deploy to AUR
+git push origin master
